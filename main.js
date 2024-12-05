fd1ecb6bf8afd60afcea5218d0ea98c77890438d
HenriqueBrasilDev <henrique_brasiloficial@outlook.com>
2024-12-05 08:03:56 -0300

    Commit organizando JS

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
+let timer = setInterval(nextSlide, 10000);
 
 function showSlide(index) {
+  clearInterval(timer); // Limpa o timer anterior antes de iniciar um novo
   slides.forEach(slide => slide.classList.remove('active'));
   slides[index].classList.add('active');
+  timer = setInterval(nextSlide, 10000); // Inicia o timer para mudar o slide a cada 5000ms (5 segundos)
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+  console.log("aaaaaaaaaa")
 }
 
 document.getElementById('next').addEventListener('click', () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
+  nextSlide();
 });
 
-document.getElementById('prev').addEventListener('click', () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-});
+// document.getElementById('prev').addEventListener('click', () => {
+//   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+//   showSlide(currentSlide);
+// });
 
-showSlide(currentSlide); // Mostra o primeiro slide ao carregar a página
+showSlide(currentSlide);
